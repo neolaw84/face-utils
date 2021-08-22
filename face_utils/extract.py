19ce8fc0cec2b9050589f3de6c9b102362f30916
Edward HHA Law <neolaw@gmail.com>
2021-08-22 10:16:59 +0000

    added all changes with tests.

--- face_utils/extract.py
+++ face_utils/extract.py
@@ -1,10 +1,16 @@
+import math
 from typing import Tuple, List
 
 import cv2
-from matplotlib.pyplot import flag
 import numpy as np
+from scipy.spatial.transform import Rotation
 
 from insightface.app import FaceAnalysis
+
+from face_utils.face_model import model_points
+from face_utils.utils import shift_centroid_to_origin as shift_to_o, scale_face as scale
+
+model_points = shift_to_o(model_points)
 
 app = FaceAnalysis(allowed_modules=["detection", "genderage", "landmark_3d_68"])
 app.prepare(ctx_id=0, det_size=(640, 640))
@@ -40,6 +46,17 @@ def _get_bbox_center_width_height(bbox:List)->Tuple:
     width, height = _get_width_and_height(bbox)
     return (bbox[X] + width/2.0, bbox[Y] + height/2.0), width, height
 
+def _rotate_image(image, angle, image_center):
+    rot_mat = cv2.getRotationMatrix2D(image_center, angle, 1.0)
+    result = cv2.warpAffine(image, rot_mat, image.shape[1::-1], flags=cv2.INTER_LINEAR)
+    return result
+
+def combine_affine_transform(M1, M2):
+    M1_ = np.vstack([M1, np.float32([[0, 0, 1]])])
+    M2_ = np.vstack([M2, np.float32([[0, 0, 1]])])
+    M_final = np.matmul(M2_, M1_)[0:2, :]
+    return M_final
+
 def _extract(img, f, size:Tuple=(256, 256), bbox_scale:float=1.0, correct_rotate:bool=False):
     bbox = f["bbox"]
 
@@ -63,10 +80,26 @@ def _extract(img, f, size:Tuple=(256, 256), bbox_scale:float=1.0, correct_rotate
     
     # correct rotation
     if correct_rotate:
-        pass
+        landmarks = f["landmark_3d_68"]
+        landmarks = shift_to_o(landmarks)
+        mps_norm = scale(model_points, landmarks)
+
+        # these two reshapes are to make sure Rotation is happy
+        mps = mps_norm.reshape((-1, 3))
+
+        # landmarks = landmarks.reshape(68*3)
+        lmk = landmarks.reshape((-1, 3))
+        rmat, rmsd = Rotation.align_vectors(a=lmk, b=mps)
+        
+        rotation = math.degrees(rmat.as_rotvec()[2])   
+
+        # rimg_r = _rotate_image(img, rotation, center)
+        M1 = cv2.getRotationMatrix2D(center, rotation, 1.0)
 
     # M = cv2.getAffineTransform(src=bbox_points, dst=_get_new_bbox_points(size=size))
     M, _ = cv2.estimateAffinePartial2D(bbox_points, _get_new_bbox_points(size=size))
+    if correct_rotate:
+        M = combine_affine_transform(M1, M)
     rimg = cv2.warpAffine(img, M, dsize=size, flags=cv2.INTER_CUBIC)
 
     return rimg
@@ -94,5 +127,26 @@ def extract(img, size:Tuple=(256, 256), bbox_scale:float=1.0, correct_rotate:boo
 
     faces = app.get(img)
     rimgs = [_extract(img, f, size=size, bbox_scale=bbox_scale, correct_rotate=correct_rotate) for f in faces]
+
+    return rimgs
+
+def _extract_pitch_yaw_roll(img, f, size:Tuple=(256, 256), bbox_scale:float=1.0, correct_rotate:bool=False):
+    bbox = f["bbox"]
+    
+    landmarks = f["landmark_3d_68"]
+    landmarks = shift_to_o(landmarks)
+    mps_norm = scale(model_points, landmarks)
+
+    # these two reshapes are to make sure Rotation is happy
+    mps = mps_norm.reshape((-1, 3))
+    lmk = landmarks.reshape((-1, 3))
+    rmat, rmsd = Rotation.align_vectors(a=lmk, b=mps)
+    
+    return rmat.as_rotvec()
+
+def extract_pitch_yaw_roll(img):
+
+    faces = app.get(img)
+    rimgs = [_extract_pitch_yaw_roll(img, f) for f in faces]
 
     return rimgs
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,10 +1,11 @@
 import pathlib
 from unittest import TestCase
 
+import numpy as np
 import requests
 import cv2
 
-from face_utils.extract import extract
+from face_utils.extract import extract, extract_pitch_yaw_roll
 
 THIS_FILE_PATH=pathlib.Path(__file__)
 THIS_DIR_PATH=THIS_FILE_PATH.parent
@@ -13,9 +14,10 @@ IMAGE_DIR_PATH=pathlib.Path.joinpath(THIS_DIR_PATH, "images")
 pathlib.Path.mkdir(IMAGE_DIR_PATH, parents=True, exist_ok=True)
 
 IMG_1="https://images.pexels.com/photos/3866555/pexels-photo-3866555.png?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260"
+IMG_2="https://images.unsplash.com/photo-1601412436009-d964bd02edbc?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=80"
 
 def _download_and_save(url:str=None, filepath:str=None)->None:
-    r = requests.get(IMG_1, allow_redirects=True)
+    r = requests.get(url, allow_redirects=True)
     if r.status_code != 200:
         raise Exception("unable to download {}".format(url))
     with open (filepath, "wb") as f:
@@ -29,6 +31,10 @@ class TestUtils(TestCase):
         if not pathlib.Path.exists(IMG_1_PATH):
             _download_and_save(url=IMG_1, filepath=IMG_1_PATH)
         self.images.append(IMG_1_PATH)
+        IMG_2_PATH = pathlib.Path.joinpath(IMAGE_DIR_PATH, "img_2.jpg")
+        if not pathlib.Path.exists(IMG_2_PATH):
+            _download_and_save(url=IMG_2, filepath=IMG_2_PATH)
+        self.images.append(IMG_2_PATH)
         return super().setUp()
 
     def tearDown(self) -> None:
@@ -42,11 +48,23 @@ class TestUtils(TestCase):
         for ip in self.images:
             img = cv2.imread(str(ip))
             rimgs = extract(img)
-            for rimg in rimgs:
+            for i, rimg in enumerate(rimgs):
                 self.assertTupleEqual(rimg.shape, (256, 256, 3))
+                cv2.imwrite(str(IMAGE_DIR_PATH) + "/result-" + str(ip) + str(i.name) + ".jpg", rimg)
                 
     def test_extract_rotate(self):
-        pass
+        for ip in self.images:
+            img = cv2.imread(str(ip))
+            rimgs = extract(img, correct_rotate=True)
+            for i, rimg in enumerate(rimgs):
+                self.assertTupleEqual(rimg.shape, (256, 256, 3))
+                cv2.imwrite(str(IMAGE_DIR_PATH) + "/result-rotate-" + str(ip.name) + str(i) + ".jpg", rimg)
 
     def test_extract_rotate_vector(self):
-        pass
+        for ip in self.images:
+            img = cv2.imread(str(ip))
+            rimgs = extract_pitch_yaw_roll(img)
+            for result in rimgs:
+                assert isinstance(result, np.ndarray)
+                assert len(result.shape) == 1
+                assert result.shape[0] == 3
